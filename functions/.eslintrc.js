c70b58bd14425c9180cf8ceda636f46fd887cc65
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-07-02 17:35:25 +0900

    Upgrade eslint lib and setting.

---
+++ functions/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': [
+    'standard'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'plugins': [
+    'babel'
+  ],
+  'parser': 'babel-eslint',
+  'rules': {
+    'no-unused-vars': 'off',
+    'no-return-assign': 'off',
+    'space-before-function-paren': ['error', 'never']
+  }
+}
